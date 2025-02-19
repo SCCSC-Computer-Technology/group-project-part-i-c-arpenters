def9cbd050739a9e6bc93cd02247c56734fb0add
Kara Crumpton <magicalmonkies@gmail.com>
2025-02-19 10:32:33 -0500

    Fixed Draft

---
+++ Group Project Draft/Data.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Group_Project_Draft
+{
+    partial class Data
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sportsStatsDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.backBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sportsStatsDBDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(559, 78);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Database will show here, filtered for \r\nwhatever sport they wanted to see\r\n";
+            // 
+            // backBtn
+            // 
+            this.backBtn.Location = new System.Drawing.Point(12, 522);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(77, 37);
+            this.backBtn.TabIndex = 1;
+            this.backBtn.Text = "Back";
+            this.backBtn.UseVisualStyleBackColor = true;
+            this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
+            // 
+            // Data
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 571);
+            this.Controls.Add(this.backBtn);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Data";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data";
+            this.Load += new System.EventHandler(this.Data_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sportsStatsDBDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource sportsStatsDBDataSetBindingSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button backBtn;
+    }
+}
---
+++ Group Project Draft/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Group_Project_Draft
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.logBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.createBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(479, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Sports Stuff";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(206, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password:";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(306, 97);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(175, 29);
+            this.nameBox.TabIndex = 3;
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(306, 146);
+            this.passBox.Name = "passBox";
+            this.passBox.Size = new System.Drawing.Size(175, 29);
+            this.passBox.TabIndex = 4;
+            this.passBox.UseSystemPasswordChar = true;
+            // 
+            // logBtn
+            // 
+            this.logBtn.Location = new System.Drawing.Point(381, 181);
+            this.logBtn.Name = "logBtn";
+            this.logBtn.Size = new System.Drawing.Size(100, 29);
+            this.logBtn.TabIndex = 5;
+            this.logBtn.Text = "Log In";
+            this.logBtn.UseVisualStyleBackColor = true;
+            this.logBtn.Click += new System.EventHandler(this.logBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(231, 275);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "New User?";
+            // 
+            // createBtn
+            // 
+            this.createBtn.Location = new System.Drawing.Point(333, 272);
+            this.createBtn.Name = "createBtn";
+            this.createBtn.Size = new System.Drawing.Size(148, 29);
+            this.createBtn.TabIndex = 7;
+            this.createBtn.Text = "Create Account";
+            this.createBtn.UseVisualStyleBackColor = true;
+            this.createBtn.Click += new System.EventHandler(this.createBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(12, 366);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(100, 29);
+            this.exitBtn.TabIndex = 8;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.logBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 407);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.createBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.logBtn);
+            this.Controls.Add(this.passBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Button logBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button createBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
