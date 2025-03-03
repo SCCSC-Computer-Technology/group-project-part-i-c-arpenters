6be4360a8db34152a73f87f95c63e446fb47d60a
Kara Crumpton <magicalmonkies@gmail.com>
2025-03-03 09:26:33 -0500

    Add files via upload

--- TestFormForGit/Form1.Designer.cs
+++ TestFormForGit/Form1.Designer.cs
@@ -37,18 +37,19 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(27, 132);
+            label1.Location = new Point(24, 99);
             label1.Name = "label1";
-            label1.Size = new Size(725, 106);
+            label1.Size = new Size(579, 86);
             label1.TabIndex = 0;
             label1.Text = "CAN YALL SEE THIS";
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(159, 342);
+            checkBox1.Location = new Point(139, 256);
+            checkBox1.Margin = new Padding(3, 2, 3, 2);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(60, 24);
+            checkBox1.Size = new Size(49, 19);
             checkBox1.TabIndex = 1;
             checkBox1.Text = "yes !";
             checkBox1.UseVisualStyleBackColor = true;
@@ -56,21 +57,24 @@
             // checkBox2
             // 
             checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(462, 342);
+            checkBox2.Location = new Point(404, 256);
+            checkBox2.Margin = new Padding(3, 2, 3, 2);
             checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(48, 24);
+            checkBox2.Size = new Size(40, 19);
             checkBox2.TabIndex = 2;
             checkBox2.Text = "no";
             checkBox2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.Yellow;
+            ClientSize = new Size(700, 338);
             Controls.Add(checkBox2);
             Controls.Add(checkBox1);
             Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
