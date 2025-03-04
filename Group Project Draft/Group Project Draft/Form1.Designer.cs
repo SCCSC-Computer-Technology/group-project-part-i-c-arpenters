60a9ceedd1048208afc2274a03f08188dc64ab36
Brandon Hines <bdhines075@ymail.com>
2025-03-04 16:23:49 -0500

    attempt to remerge
    maybe this works? hopefully

---
+++ Group Project Draft/Group Project Draft/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Group_Project_Draft
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.logBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.createBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(606, 62);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Sports Stuff";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password:";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(199, 93);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(175, 34);
+            this.nameBox.TabIndex = 3;
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(199, 142);
+            this.passBox.Name = "passBox";
+            this.passBox.Size = new System.Drawing.Size(175, 34);
+            this.passBox.TabIndex = 4;
+            this.passBox.UseSystemPasswordChar = true;
+            // 
+            // logBtn
+            // 
+            this.logBtn.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.logBtn.ForeColor = System.Drawing.Color.Black;
+            this.logBtn.Location = new System.Drawing.Point(143, 212);
+            this.logBtn.Name = "logBtn";
+            this.logBtn.Size = new System.Drawing.Size(104, 38);
+            this.logBtn.TabIndex = 5;
+            this.logBtn.Text = "Log In";
+            this.logBtn.UseVisualStyleBackColor = false;
+            this.logBtn.Click += new System.EventHandler(this.logBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(153, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 27);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "New User?";
+            // 
+            // createBtn
+            // 
+            this.createBtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.createBtn.Location = new System.Drawing.Point(110, 334);
+            this.createBtn.Name = "createBtn";
+            this.createBtn.Size = new System.Drawing.Size(137, 41);
+            this.createBtn.TabIndex = 7;
+            this.createBtn.Text = "Create Account";
+            this.createBtn.UseVisualStyleBackColor = true;
+            this.createBtn.Click += new System.EventHandler(this.createBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.exitBtn.Location = new System.Drawing.Point(110, 381);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(137, 46);
+            this.exitBtn.TabIndex = 8;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.logBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(847, 451);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.createBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.logBtn);
+            this.Controls.Add(this.passBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Button logBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button createBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ Group Project Draft/Group Project Draft/Worm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Group_Project_Draft
+{
+    partial class Worm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Worm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(218, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(715, 106);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Worm Charming";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(242, 254);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(346, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(567, 243);
+            this.label2.TabIndex = 7;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(292, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(810, 191);
+            this.label3.TabIndex = 8;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 459);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 46);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Worm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 571);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Worm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Worm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
